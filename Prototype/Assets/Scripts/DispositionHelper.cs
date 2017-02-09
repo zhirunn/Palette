0560ce85cdc92b0eee7ce25322fbe4d054f192cf
David Yee <dvyee@ualberta.ca>
2017-02-08 17:15:46 -0700

    Add DispositionController
    - Handles the change in particle colours as a character's disposition
      changes

---
+++ Prototype/Assets/Scripts/DispositionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DispositionHelper {
+    private static Color ORANGE = new Color32(255, 140, 0, 255);
+
+    /// <summary>
+    /// Get the colour from a disposition amount between 0 and 100.
+    /// </summary>
+    /// <param name="disposition"></param>
+    /// <returns></returns>
+    public static Color getColor(int disposition)
+    {
+        if(disposition >= 60)
+        {
+            return Color.blue;
+        } else if(disposition >=40)
+        {
+            return Color.gray;
+        } else
+        {
+            return ORANGE;
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/Enemies/DispositionController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispositionController : MonoBehaviour {
+    [Range(0, 100)]
+    [SerializeField]
+    private int _disposition;
+    public int disposition
+    {
+        get { return _disposition;  }
+        set
+        {
+            _disposition = value;
+            
+            if(psr != null)
+            {
+                Color color = DispositionHelper.getColor(value);
+                psr.material.SetColor("_EmissionColor", color);
+            }
+        }
+    }
+
+    private ParticleSystem ps;
+    private ParticleSystemRenderer psr;
+
+	// Use this for initialization
+	void Start () {
+        ps = GetComponent<ParticleSystem>();
+        psr = GetComponent<ParticleSystemRenderer>();
+        disposition = _disposition;
+        // psr.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    // Update the color when changed in the Unity editor
+    void OnValidate()
+    {
+        disposition = _disposition;
+    }
+}
