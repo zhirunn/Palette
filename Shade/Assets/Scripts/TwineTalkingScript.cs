1295dcc8182d109aa6672722a6443a7afa76c6d0
zhirunn <zhirunn@gmail.com>
2017-02-11 22:19:00 -0700

    It kinda works but right now it's hardcoded in to only work for the girl object. Gotta find a way to make it work in some other way

--- Shade/Assets/Scripts/TwineTalkingScript.cs
+++ Shade/Assets/Scripts/TwineTalkingScript.cs
@@ -2,26 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TalkingScript : MonoBehaviour {
+public class TwineTalkingScript : MonoBehaviour {
 
 	public TwineTextPlayer textPlayer;
-
+	bool characterInRange = false;
 	bool _Seetext = false;
 
 	void OnTriggerEnter2D(Collider2D other) {
-   	//characterInRange = true;
+		if(other.name == "girl") {
+   		characterInRange = true;
+   	}
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.name == "girl") {
+   		characterInRange = false;
+   	}
 	}
 	
 	void Update(){
 		bool BtnPressed = Input.GetButtonDown("TwineText");
-		if(BtnPressed) {
+		if(characterInRange == false) {
+			_Seetext = false;		
+		}
+		if((BtnPressed) && (characterInRange == true)) {
 			_Seetext = true;
 		}
 		if(_Seetext == true) {
 			textPlayer.gameObject.SetActive(true);
 			//textPlayer.gameObject.GetComponent<TwineTextPlayer>().StartStory = true;
 		}
-		else{
+		else if(_Seetext == false) {
 			textPlayer.gameObject.SetActive(false);
 		}
 	}
