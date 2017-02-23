695df8e9426301e3d5faf66f17c57e7fb24f0e46
zhirunn <zhirunn@gmail.com>
2017-02-22 22:43:09 -0700

    implemented unlocking sound effects

--- Shade/Assets/Scripts/Level2/opendoor.cs
+++ Shade/Assets/Scripts/Level2/opendoor.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class opendoor : MonoBehaviour {
 
     // Use this for initialization
-    void OnTriggerEnter2D(Collider2D col) {
+	IEnumerator OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.tag == "door") {
-            
+			AudioSource unlockSound = GetComponent<AudioSource>();
+			unlockSound.Play();
+			yield return new WaitForSeconds(0.9f);
             Destroy(col.gameObject, 0.3f);
             Destroy(this.gameObject, 0.1f);
         }
     }
-    
 }
