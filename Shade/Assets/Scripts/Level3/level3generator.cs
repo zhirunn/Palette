0d09f07cd4feec4bfc8be67be2b41ce8f18971c8
Zhengo <gilbert.liu@rocketmail.com>
2017-03-02 22:06:45 -0700

    Added some place fillers script for level3

---
+++ Shade/Assets/Scripts/Level3/level3generator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level3generator : MonoBehaviour {
+
+    public GameObject tileprefab1;
+    public GameObject tileprefab2;
+    public GameObject tileprefab3;
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void SpawnNextRoom(){
+
+        }
+}
