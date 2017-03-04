26d1f298efd2a4eb05cdf602adf3aee1859600be
Zhengo <gilbert.liu@rocketmail.com>
2017-03-03 21:29:24 -0700

    Some Code Compeleted

--- Shade/Assets/Scripts/Level3/level3generator.cs
+++ Shade/Assets/Scripts/Level3/level3generator.cs
@@ -8,10 +8,16 @@ public class level3generator : MonoBehaviour {
     public GameObject tileprefab2;
     public GameObject tileprefab3;
 
-    
+    List<GameObject> all_tiles;    
 	// Use this for initialization
 	void Start () {
-		
+        all_tiles = new List<GameObject>();
+
+        for (int i = 0; i < 3; i++) {
+            GameObject obj = (GameObject) Instantiate(tileprefab1);
+            obj.SetActive(false);
+            all_tiles.Add(obj);
+        }
 	}
 	
 	// Update is called once per frame
@@ -20,5 +26,20 @@ public class level3generator : MonoBehaviour {
 	}
     public void SpawnNextRoom(){
 
+
+    }
+
+    public GameObject GetUsableObject() {
+        for (int i = 0; i < all_tiles.Count;i++ ) {
+            if (!all_tiles[i].activeInHierarchy)
+            {
+                return all_tiles[i];
+            }
         }
+        GameObject obj = (GameObject)Instantiate(tileprefab1);
+        obj.SetActive(false);
+        all_tiles.Add(obj);
+        return obj;
+
+    }
 }
---
+++ Shade/Assets/Scripts/Level3/platformControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformControl : MonoBehaviour {
+
+    public GameObject recyclePoint;
+
+    public level3generator lv3gen;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+        if (transform.position.y >= recyclePoint.transform.position.y) {
+            this.gameObject.SetActive(false);
+        }
+	}
+
+    private void move() {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.03f);
+    }
+
+}
