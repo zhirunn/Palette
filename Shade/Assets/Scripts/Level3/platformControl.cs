69ce20f167b1cfec87828846ce0685163a647390
Zhengo <gilbert.liu@rocketmail.com>
2017-03-04 12:40:27 -0700

    some fixes, not completed yet

--- Shade/Assets/Scripts/Level3/platformControl.cs
+++ Shade/Assets/Scripts/Level3/platformControl.cs
@@ -9,19 +9,19 @@ public class platformControl : MonoBehaviour {
     public level3generator lv3gen;
 	// Use this for initialization
 	void Start () {
-		
+        
+        lv3gen.all_tiles.Add(this.gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        move();
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.03f);
         if (transform.position.y >= recyclePoint.transform.position.y) {
+            
             this.gameObject.SetActive(false);
+
+            lv3gen.SpawnNextRoom(lv3gen.all_tiles);
+
         }
 	}
-
-    private void move() {
-        transform.position = new Vector3(transform.position.x, transform.position.y + 0.03f);
-    }
-
 }
