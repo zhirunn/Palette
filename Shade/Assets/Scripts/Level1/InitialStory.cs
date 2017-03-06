871be85ad237bf5eda46397776cf35f9dfdb4617
will1029 <william-qw@hotmail.com>
2017-03-06 15:39:39 -0700

    AddedMovementToDoc

--- Shade/Assets/Scripts/Level1/InitialStory.cs
+++ Shade/Assets/Scripts/Level1/InitialStory.cs
@@ -8,18 +8,50 @@ using UnityEngine;
 public class InitialStory : MonoBehaviour {
 
     public GameObject doc;
+    public float speed;
+    public Transform movePosition;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         //Dialouge here
         print("Dialouge Here");
 
-        //Move Dr.Evian out the door
+        //Move Dr.Evian out the door 
+        moveDoc();
         //Play sounds
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         this.GetComponent<Collider2D>().enabled = false;
+    }
+
+    void moveDoc()
+    {
+        //doc.transform.position = Vector3.MoveTowards(doc.transform.position, movePosition.position, Time.deltaTime * speed);
+
+        StartCoroutine(MoveOverSeconds());
+    }
+
+    public IEnumerator MoveOverSpeed()
+    {
+        while (doc.transform.position != movePosition.position)
+        {
+            doc.transform.position = Vector3.MoveTowards(doc.transform.position, movePosition.position, Time.deltaTime * speed);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public IEnumerator MoveOverSeconds()
+    {
+        float elapsedTime = 0;
+        Vector3 startingPos = doc.transform.position;
+        while (elapsedTime < speed)
+        {
+            doc.transform.position = Vector3.Lerp(startingPos, movePosition.position, (elapsedTime / speed));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        doc.transform.position = movePosition.position;
     }
 }
