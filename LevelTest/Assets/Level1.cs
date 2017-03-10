0930f09680f5bee3d62fdf6a5398eb26b8a7aba6
will1029 <william-qw@hotmail.com>
2017-03-10 01:02:57 -0700

    Added Preliminary Level 1 Event Handler

--- LevelTest/Assets/Level1.cs
+++ LevelTest/Assets/Level1.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Level1 : MonoBehaviour {
+    public GameObject room1;
+    public GameObject room2;
+    public GameObject room3;
+    public GameObject room4;
+    public GameObject doors;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        disableSprite(room1);
+        disableSprite(room2);
+        disableSprite(room3);
+        disableSprite(room4);
+        disableSprite(doors);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Vision"))
+        {
+            //Show rooms
+        }
+    }
+
+    //Disable sprite renderers of all child objects
+    void disableSprite(GameObject obj)
+    {
+        foreach (SpriteRenderer renderer in obj.GetComponentsInChildren<SpriteRenderer>())
+        {
+            renderer.enabled = false;
+        }
+    }
 }
