1db5c25e567d4d9e21dd66016ca5738f82e1498a
Zhengo <gilbert.liu@rocketmail.com>
2017-03-10 22:36:32 -0700

    Test on Timeslowing effect

---
+++ Shade/Assets/timetest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timetest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleInput();
+    }
+    private void HandleInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            Time.timeScale = 0.05f;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            Time.timeScale = 1f;
+        }
+        Time.fixedDeltaTime = 0.02f * Time.deltaTime;
+    }
+}
