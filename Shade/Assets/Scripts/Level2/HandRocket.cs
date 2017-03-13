74fbe0cdec2dbc8ed326cc2c0febfd6b847d82e1
Zhengo <gilbert.liu@rocketmail.com>
2017-03-12 19:18:50 -0600

    TimeSLowing effect for boss battle

--- Shade/Assets/Scripts/Level2/HandRocket.cs
+++ Shade/Assets/Scripts/Level2/HandRocket.cs
@@ -4,38 +4,36 @@ using UnityEngine;
 
 public class HandRocket : MonoBehaviour {
 
+
+    public int rotOffset;
     public GameObject Hand_front;
     public GameObject Hand_back;
     public GameObject Hand_Left;
     public GameObject Hand_Right;
 
-    private SkinnedMeshRenderer front_hand;
-    private SkinnedMeshRenderer back_hand;
-    private SkinnedMeshRenderer left_hand;
-    private SkinnedMeshRenderer right_hand;
+    //private SkinnedMeshRenderer front_hand;
+  //  private SkinnedMeshRenderer back_hand;
+   // private SkinnedMeshRenderer left_hand;
+ //   private SkinnedMeshRenderer right_hand;
 
     // Use this for initialization
     void Start () {
-        front_hand = Hand_front.GetComponent<SkinnedMeshRenderer>();
-        back_hand = Hand_back.GetComponent<SkinnedMeshRenderer>();
-        left_hand = Hand_Left.GetComponent<SkinnedMeshRenderer>();
-        right_hand = Hand_Right.GetComponent<SkinnedMeshRenderer>();
+     //   front_hand = Hand_front.GetComponent<SkinnedMeshRenderer>();
+    //    back_hand = Hand_back.GetComponent<SkinnedMeshRenderer>();
+    //    left_hand = Hand_Left.GetComponent<SkinnedMeshRenderer>();
+    //    right_hand = Hand_Right.GetComponent<SkinnedMeshRenderer>();
 		// Hand = Find("HarpoonHand")
         // Hand.setActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        HandleInput();
-	}
+        //HandleInput();
+        AimDirection();
+    }
 
     void HandleInput() {
-        if (Input.GetKeyDown("fire2")) {
-            AimDirection();
-        }
-        if (Input.GetKeyUp("fire2")) {
-            //shoot
-        }
+        
     }
     public void AimDirection()
     {
@@ -43,6 +41,6 @@ public class HandRocket : MonoBehaviour {
         difference.Normalize();
 
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + 1f);
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotOffset);
     }
 }
--- Shade/Assets/Scripts/Level3/Aim.cs
+++ Shade/Assets/Scripts/Level3/Aim.cs
@@ -5,23 +5,47 @@ using UnityEngine;
 public class Aim : MonoBehaviour {
 
     public int rotOffset;
-	// Use this for initialization
+    //public Camera cam;
+    // Use this for initialization
+    private Animator m_anim;
+    
+
+
 	void Start () {
-		
+        //cam = FindObjectOfType<Camera>();
+        m_anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        AimDirection();
+        HandleInput();
 	}
     public void AimDirection()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        difference.Normalize();
+        //  Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //  Vector3 difference = Input.mousePosition - transform.position;
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = pos - transform.position;
+        direction.Normalize();
 
-
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotOffset);
         
+    }
+    private void HandleInput() {
+        if (Input.GetKeyDown(KeyCode.L)) {
+            Time.timeScale = 0.03f;
+        }
+        if (Input.GetKeyUp(KeyCode.L)) {
+            m_anim.SetTrigger("Fire");
+            Time.timeScale = 1f;
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
     }
 }
