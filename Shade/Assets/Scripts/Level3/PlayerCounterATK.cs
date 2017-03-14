6c2fbde737ce7a59056cf457dfb01cc863584d59
Zhengo <gilbert.liu@rocketmail.com>
2017-03-13 20:55:13 -0600

    CounterAttackDone!

---
+++ Shade/Assets/Scripts/Level3/PlayerCounterATK.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCounterATK : MonoBehaviour {
+
+
+    public List<GameObject> Spikes;
+    public Animator m_anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.K)) {
+            if (Spikes.Count > 0) {
+                foreach (GameObject item in Spikes)
+                {
+                    item.GetComponent<spikeControl>().speed = 0f;
+                }
+                Time.timeScale = 0.1f;
+                m_anim.SetTrigger("Swipe");
+            }
+
+        }
+        if (Input.GetKeyUp(KeyCode.K)) {
+            Time.timeScale = 1f;
+            foreach (GameObject item in Spikes){
+                item.transform.Rotate(0, 0, 180f);
+                item.GetComponent<spikeControl>().speed = 5f;
+            }
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "spike")
+        {
+            Spikes.Add(col.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "spike")
+        {
+            Spikes.Remove(collision.gameObject);
+        }
+    }
+}
