3107ce7b58294e1eb7b6bf1523f884cfec2f38d6
Zhengo <gilbert.liu@rocketmail.com>
2017-03-13 21:44:51 -0600

    BossFight fixes

--- Shade/Assets/Scripts/Level3/BossControl.cs
+++ Shade/Assets/Scripts/Level3/BossControl.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BossControl : MonoBehaviour {
 
-    public float HP;
+    public int HP;
     public List<GameObject> Spikes_prefab;
     public GameObject Hand1_prefab;
     public GameObject Hand2_prefab;
 	// Use this for initialization
 	void Start () {
-        HP = 100f;
+        HP = 20;
 
 	}
 	
---
+++ Shade/Assets/Scripts/Level3/spikeControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeControl : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        speed = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, -Time.deltaTime * speed, 0);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "BOSS") {
+            collision.GetComponent<BossControl>().HP -= 1;
+            Destroy(this.gameObject, 1f);
+            // Explode
+        }
+        if (collision.tag == "Player") {
+            collision.GetComponent<Player>().health -= 20f;
+            Destroy(this.gameObject, 1f);
+            // Explode
+        }
+        if (collision.tag == "ATKbox") {
+            Destroy(this.gameObject, 1f);
+        }
+        
+    }
+}
