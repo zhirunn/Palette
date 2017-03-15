4add33c9fc7e31a948b8cfe71583c44426ab9c37
will1029 <william-qw@hotmail.com>
2017-03-15 10:13:24 -0600

    SomeScriptChanges

--- Shade/Assets/Scripts/Level1/Room1.cs
+++ Shade/Assets/Scripts/Level1/Room1.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Room1 : MonoBehaviour {
+    public GameObject spawner;
+    //public Transform[] spawnPoints;
+    public GameObject enemyToSpawn;
+    public Transform[] enemyMarkers;
 
-	// Use this for initialization
-	void Start () {
+    public int numEnemy;
+
+    // Use this for initialization
+    void Start () {
         
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player") { return; }
+
+        //TODO: Room mod
+
+        spawner.GetComponent<Spawn>().enemyMarkers = this.enemyMarkers;
+        spawner.GetComponent<Spawn>().spawnPoint = this.enemyMarkers[2];
+        spawner.GetComponent<Spawn>().enemyToSpawn = this.enemyToSpawn;
+    }
 }
