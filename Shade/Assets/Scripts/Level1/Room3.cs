38f1bbfa909675a5f9c8b95e50ebb8ea1be68661
will1029 <william-qw@hotmail.com>
2017-03-16 02:53:23 -0600

    Room 1 and 2 are almost done

--- Shade/Assets/Scripts/Level1/Room3.cs
+++ Shade/Assets/Scripts/Level1/Room3.cs
@@ -2,29 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room1 : MonoBehaviour {
+public class Room3 : MonoBehaviour {
+
     public GameObject spawner;
     public GameObject enemyToSpawn;
-
     public int totalEnemy;
 
-    public Transform[] spawnPoints;
-    public GameObject passThrough;
-    
+    private Transform[] spawnPoints;
+    private GameObject[] markers;
+
+    void onStart()
+    {
+        markers = GameObject.FindGameObjectsWithTag("Room2Spawn");
+        spawnPoints = new Transform[2];
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag != "Player") { return; }
 
         //TODO: Room mod
 
+
         for (int i = 0; i < totalEnemy; i++)
         {
             Spawn enemySpawn = spawner.GetComponent<Spawn>();
 
+            spawnPoints.SetValue(markers[0].GetComponent<Transform>(), i);
+            spawnPoints.SetValue(markers[1].GetComponent<Transform>(), i + 1);
+            spawnPoints.SetValue(markers[2].GetComponent<Transform>(), i + 2);
+
             enemySpawn.setEnemyMarkers(spawnPoints);
             enemySpawn.GetComponent<Spawn>().setEnemy(enemyToSpawn);
 
-            if (i >= spawnPoints.Length) {
+            if (i >= spawnPoints.Length)
+            {
                 enemySpawn.GetComponent<Spawn>().setSpawnPoint(spawnPoints[i % spawnPoints.Length]);
             }
             else
@@ -35,9 +47,7 @@ public class Room1 : MonoBehaviour {
             enemySpawn.GetComponent<Spawn>().spawn();
         }
 
-        passThrough.GetComponent<BoxCollider2D>().enabled = false;
-
         this.GetComponent<BoxCollider2D>().enabled = false;
-        
+
     }
 }
