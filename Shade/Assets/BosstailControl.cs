22f0b8cf83633257738940b47ff7f40420c15d34
Zhengo <gilbert.liu@rocketmail.com>
2017-03-15 22:58:01 -0600

    some fixes

--- Shade/Assets/BosstailControl.cs
+++ Shade/Assets/BosstailControl.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class BosstailControl : MonoBehaviour {
 
+    public GameObject target;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () { 
+        transform.position = new Vector3 (target.transform.position.x, transform.position.y, transform.position.z);
 	}
 }
