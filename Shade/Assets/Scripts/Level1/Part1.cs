607ee303e5f11089660e43bd1ed7afa8d00753a6
will1029 <william-qw@hotmail.com>
2017-03-18 18:07:00 -0600

    Room1Fixes

---
+++ Shade/Assets/Scripts/Level1/Part1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part1 : MonoBehaviour {
+    public GameObject doors;
+    public GameObject fakeDoors;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player") { return; }
+
+        //Disable doors
+        foreach (SpriteRenderer renderer in doors.GetComponentsInChildren<SpriteRenderer>())
+        {
+            renderer.enabled = false;
+        }
+        foreach (BoxCollider2D collider in doors.GetComponentsInChildren<BoxCollider2D>())
+        {
+            collider.enabled = false;
+        }
+
+        //Enable triggers
+        foreach (PolygonCollider2D collider in fakeDoors.GetComponentsInChildren<PolygonCollider2D>())
+        {
+            collider.isTrigger = true;
+        }
+
+        this.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+}
