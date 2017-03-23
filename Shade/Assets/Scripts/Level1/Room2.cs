8b4f3bc2f180ba7956867713ccc735298002a004
will1029 <william-qw@hotmail.com>
2017-03-23 12:05:53 -0600

    Merge

--- Shade/Assets/Scripts/Level1/Room2.cs
+++ Shade/Assets/Scripts/Level1/Room2.cs
@@ -11,16 +11,7 @@ public class Room2 : MonoBehaviour {
     public Transform[] spawnPoints;
     public GameObject passThrough;
 
-    //private GameObject[] spawnObjects;
-
-    /*
-    void onStart()
-    {
-        spawnObjects = GameObject.FindGameObjectsWithTag("Level1Spawn");
-        spawnPoints.SetValue(spawnObjects[0].GetComponent<Transform>(), 0);
-   
-    }
-    */
+    private Transform[] temp;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -31,13 +22,42 @@ public class Room2 : MonoBehaviour {
         for (int i = 0; i < totalEnemy; i++)
         {
             Spawn enemySpawn = spawner.GetComponent<Spawn>();
-
-            enemySpawn.setEnemyMarkers(spawnPoints);
-            enemySpawn.GetComponent<Spawn>().setSpawnPoint(spawnPoints[i]);
             enemySpawn.GetComponent<Spawn>().setEnemy(enemyToSpawn);
+
+            temp = new Transform[3];
+
+            //Set enemy patrol path
+            int mark1 = Random.Range(0, spawnPoints.Length);
+            int mark2 = Random.Range(0, spawnPoints.Length);
+            int mark3 = Random.Range(0, spawnPoints.Length);
+
+            //Choose patrol path
+            while ((mark1 == mark2) && (mark2 == mark3))
+            {
+                mark1 = Random.Range(0, spawnPoints.Length);
+                mark2 = Random.Range(0, spawnPoints.Length);
+            }
+
+            //Set markers
+            temp[0] = spawnPoints[mark1];
+            temp[1] = spawnPoints[mark2];
+            temp[2] = spawnPoints[mark3];
+            enemySpawn.setEnemyMarkers(temp);
+
+            //Cycle through spawn points to spawn monsters.
+            if (i >= spawnPoints.Length)
+            {
+                enemySpawn.GetComponent<Spawn>().setSpawnPoint(spawnPoints[i % spawnPoints.Length]);
+            }
+            else
+            {
+                enemySpawn.GetComponent<Spawn>().setSpawnPoint(spawnPoints[i]);
+            }
+
             enemySpawn.GetComponent<Spawn>().spawn();
         }
 
         passThrough.GetComponent<BoxCollider2D>().enabled = false;
+        this.GetComponent<BoxCollider2D>().enabled = false;
     }
 }
