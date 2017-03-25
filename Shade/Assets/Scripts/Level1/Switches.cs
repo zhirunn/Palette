3feca378e466cdea7067b28a814d1395ff8e7e4c
will1029 <william-qw@hotmail.com>
2017-03-25 15:56:16 -0600

    Level1Pt2

---
+++ Shade/Assets/Scripts/Level1/Switches.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour {
+    private bool state = true;
+
+	public void enable()
+    {
+        this.GetComponent<CircleCollider2D>().enabled = true;
+        this.GetComponent<SpriteRenderer>().enabled = true;
+        state = true;
+    }
+
+    public void disable()
+    {
+        this.GetComponent<CircleCollider2D>().enabled = false;
+        this.GetComponent<SpriteRenderer>().enabled = false;
+        state = false;
+    }
+
+    public bool getState()
+    {
+        return (state);
+    }
+}
