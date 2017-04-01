702e1af5472c2603c5d1e67c3a57edce813b7e55
Zhengo <gilbert.liu@rocketmail.com>
2017-03-31 21:46:09 -0600

    Added transition to Level3

---
+++ Shade/Assets/Lv3IntroBossCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lv3IntroBossCtrl : MonoBehaviour {
+
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool("battle", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
