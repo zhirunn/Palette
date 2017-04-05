9631395af9b483bba82d83a8444c2d740e2d5771
Zhengo <gilbert.liu@rocketmail.com>
2017-04-04 21:55:24 -0600

    Added Boss Settlement

---
+++ Shade/Assets/EndingAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingAnimator : MonoBehaviour {
+    public Animator Player;
+    public Animator Boss;
+	// Use this for initialization
+	void Start () {
+        Boss.SetBool("Battle", false);
+        Boss.SetTrigger("Death");
+        Boss.SetBool("dead", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
