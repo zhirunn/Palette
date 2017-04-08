90f852b2d4400f7b26afde3436897cd5810951a3
will1029 <william-qw@hotmail.com>
2017-04-08 15:45:46 -0600

    Added grab

---
+++ Shade/Assets/Scripts/Level2V3/phoneStop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class phoneStop : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Reward")
+        {
+            col.transform.parent = this.transform;
+            col.GetComponent<CircleCollider2D>().isTrigger = false;
+        }
+    }
+}
