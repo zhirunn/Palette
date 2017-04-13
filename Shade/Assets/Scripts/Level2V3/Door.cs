0f658b6c02cec23f5218a1e7a71a674b2a789a29
will1029 <william-qw@hotmail.com>
2017-04-12 19:53:22 -0600

    Changes

---
+++ Shade/Assets/Scripts/Level2V3/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    public GameObject phoneParts;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        try
+        {
+            phoneParts.GetComponentInChildren<BoxCollider2D>().enabled = false;
+        } catch
+        {
+            this.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
